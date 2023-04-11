1a4526390e543f2050c00c1baf60527913911b2b
Tonko <tbosse20@student.aau.dk>
2023-04-11 11:33:37 +0200

    Copy, paste, meow sounds

--- GD mini/Assets/Scripts/LevelComplete.cs
+++ GD mini/Assets/Scripts/LevelComplete.cs
@@ -7,10 +7,17 @@ public class LevelComplete : MonoBehaviour {
 
     public string nextLevelNr;
 
+    public AudioSource MeowAS;
+
     private void OnTriggerEnter(Collider col) {
-        if (col.tag == "Player") {
-            string nextLevel = "Level " + nextLevelNr;
-            SceneManager.LoadScene(nextLevel);
-        }
+        if (col.tag != "Player") return; 
+        StartCoroutine(PlayEndSceneSound());
     }
+
+    IEnumerator PlayEndSceneSound(){
+        MeowAS.Play();
+        yield return new WaitWhile (()=> MeowAS.isPlaying);
+        string nextLevel = "Level " + nextLevelNr;
+        SceneManager.LoadScene(nextLevel);
+     }
 }
