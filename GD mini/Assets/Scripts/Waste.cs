be53624723804c4b4950e1b1a5e0649289553fa9
Tonko <tbosse20@student.aau.dk>
2023-04-11 12:28:59 +0200

    Waste, player

--- GD mini/Assets/Scripts/Waste.cs
+++ GD mini/Assets/Scripts/Waste.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Waste : MonoBehaviour {
+
+    public AudioSource WasteAS;
+
     void OnTriggerStay(Collider collider) {
         if (collider.transform.tag != "Player") return;
-        new Menu().ReloadScene();
     }
+    
+    IEnumerator PlayEndSceneSound(){
+        WasteAS.Play();
+        yield return new WaitWhile (()=> WasteAS.isPlaying);
+        new Menu().ReloadScene();
+     }
 }
